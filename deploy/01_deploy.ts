4d613fc244c47acfab55e8c02fd54722a62f8f43
Kai Aldag <kaialdag@icloud.com>
2022-09-28 01:59:36 +0200

    couple blobs n shit

---
+++ deploy/01_deploy.ts
@@ -0,0 +1,12 @@
+
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+
+const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const { ethers } = hre;
+    const Blobs = await ethers.getContractFactory("Blobs");
+    const tx = await Blobs.deploy("Blobs", "BLB");
+    console.log(tx);
+}
+
+export default deploy;
