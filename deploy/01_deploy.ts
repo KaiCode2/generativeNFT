a5fdde9d66b58df342d2f7893897482206ab5a3a
Kai Aldag <kaialdag@icloud.com>
2022-10-04 23:56:36 +0200

    Finally got it to goerli. Getting base64 issues with webgl shaders

--- deploy/01_deploy.ts
+++ deploy/01_deploy.ts
@@ -3,16 +3,21 @@ import { DeployFunction } from "hardhat-deploy/types";
 
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { ethers } = hre;
-  const Blobs = await ethers.getContractFactory("Blobs");
-  const tx = await Blobs.deploy("Blobs", "BLB");
+  const Blobs = await ethers.getContractFactory("Diamonds");
+  const tx = await Blobs.deploy("Diamonds", "DMD");
   console.log(tx);
 
   await tx.deployTransaction.wait();
 
-  await hre.run("verify:verify", {
-    address: tx.address,
-    constructorArguments: ["Blobs", "BLB"],
-  });
+  if (hre.network.name == "goerli" || hre.network.name == "mainnet") {
+    // await hre.run("verify:verify", {
+    //     address: tx.address,
+    //     constructorArguments: ["Diamonds", "DMD"],
+    //   });
+  } else {
+    await tx.mint(0);
+    console.log(await tx.tokenURI(0));
+  }
 };
 
 export default deploy;
